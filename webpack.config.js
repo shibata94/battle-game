0b18b6ea62c293fccb0ea3b0a566212a88d288b0
shibata94 <mnm0904.choco@gmail.com>
2025-09-28 19:52:34 +0900

    コンフィグ変更

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
-        publicPath: 'dist/',
+        publicPath: './',
     },
     devServer: {
         publicPath: '/dist/',
